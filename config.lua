ebe52ab1c679cc28127d288871967bb2b3dce173
vpeopleonatank <vpeopleonatank@gmail.com>
2021-08-30 15:15:12 +0700

    add color to indent_blankline

--- config.lua
+++ config.lua
@@ -357,13 +357,16 @@ lvim.plugins = {
 	{
 		"lukas-reineke/indent-blankline.nvim",
     event = "BufRead",
-    setup = function()
-      vim.g.indentLine_enabled = 1
-      vim.g.indent_blankline_char = "▏"
-      vim.g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard"}
-      vim.g.indent_blankline_buftype_exclude = {"terminal"}
-      vim.g.indent_blankline_show_trailing_blankline_indent = false
-      vim.g.indent_blankline_show_first_indent_level = false
+    config = function()
+      require("indent_blankline").setup({
+        char = "▏",
+        space_char_blankline = " ",
+        filetype_exclude = { "dashboard" },
+        show_trailing_blankline_indent = false,
+        buftype_exclude = { "help", "terminal", "nofile" },
+      })
+
+      vim.cmd("highlight IndentBlanklineChar guifg=#666B70 gui=nocombine")
     end
 	},
 	-- {
