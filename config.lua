a401e59a6ff732d0004bbe9ec145f7971d4b2a32
vpeopleonatank <vpeopleonatank@gmail.com>
2021-08-31 14:10:31 +0700

    disable treesitter 's indent

--- config.lua
+++ config.lua
@@ -214,7 +214,7 @@ lvim.builtin.nvimtree.hide_dotfiles = 0
 -- Treesitter
 -- *
 lvim.builtin.treesitter.ensure_installed = "maintained"
--- lvim.builtin.treesitter.indent.enable = false
+lvim.builtin.treesitter.indent.enable = false
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 lvim.builtin.treesitter.matchup.enable = true
